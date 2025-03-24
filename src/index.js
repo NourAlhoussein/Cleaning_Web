d94fdb31ec55d6920afd60e5acb71649a3ed6473
nour alhoussein <nour.alhoussein@axtro-soft.com>
2025-03-24 14:28:24 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom"; // Import HashRouter
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// Use ReactDOM.render to render the App component
+ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
